a7612856c9a0841555221e5e162b72898d1cde81
Daban Sadiq <dabansadiq@icloud.com>
2021-06-15 15:41:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ class Formatter {
     let result = [];
     let arrayOfWords = sentence.split( " " )
     for ( let n = 0; n < arrayOfWords.length; n++ ) {
-      if ( n == 0 ) {
-        result.push( this.capitalize( arrayOfWords[ n ] ) )
-      } else {
-        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
-          result.push( arrayOfWords[ n ] )
-        } else {
-          result.push( this.capitalize( arrayOfWords[ n ] ) )
-        }
-      }
-
-    }
+    //   if ( n == 0 ) {
+    //     result.push( this.capitalize( arrayOfWords[ n ] ) )
+    //   } else {
+    //     if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+    //       result.push( arrayOfWords[ n ] )
+    //     } else {
+    //       result.push( this.capitalize( arrayOfWords[ n ] ) )
+    //     }
+    //   }
+    //
+    // }
     return result.join( " " );
   }
 }
